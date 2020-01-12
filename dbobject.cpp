a46276d05f40869b24b913745fce5a4a68e85fd6
Tomasz Szczodrowski <bdn583@gmail.com>
2020-01-12 11:11:11 +0100

    Add DbObject

---
+++ dbobject.cpp
@@ -0,0 +1,28 @@
+#include <string>
+
+class DbObject {
+    int id = -1;
+    bool isIdSet = false;
+    std::string name;
+
+    public: DbObject(std::string name) {
+        this->name = name;
+    }
+
+    public: int getId() {
+        return id;
+    }
+
+    public: void setId(int id) {
+        if(!isIdSet) {
+            isIdSet = true;
+            this->id = id;
+        } else {
+            throw -1;
+        }
+    }
+
+    public: std::string getName() {
+        return name;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,10 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include "lib/catch.hpp"
 #include "ainteger.cpp"
+#include "dbobject.cpp"
 
 TEST_CASE("Atomic Integer works") {
     AtomicInteger atomicInteger = AtomicInteger();
     REQUIRE( atomicInteger.getAndIncrement() == 0 );
     REQUIRE( atomicInteger.getAndIncrement() == 1 );
     REQUIRE( atomicInteger.getAndIncrement() == 2 );
+}
+
+TEST_CASE("Db Object works") {
+    DbObject dbObject = DbObject("My object");
+    REQUIRE( dbObject.getName() == "My object" );
+    REQUIRE( dbObject.getId() == -1 );
+    dbObject.setId(12);
+    REQUIRE( dbObject.getId() == 12 );
+    CHECK_THROWS(dbObject.setId(13));
+    // TODO set id, check id, verify that id cant be set again
 }
